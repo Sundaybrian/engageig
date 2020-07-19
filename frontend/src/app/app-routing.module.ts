bbf65060b4e2e051f984d8408756126a74d6a1c9
Brian Sunday <brians931@gmail.com>
2020-07-19 23:32:56 +0300

    setting up routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginpageComponent } from "./pages/loginpage/loginpage.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { RegisterpageComponent } from "./pages/registerpage/registerpage.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: DashboardComponent,
+  },
+  { path: "login", component: LoginpageComponent },
+  { path: "register", component: RegisterpageComponent },
+  { path: "logout", component: LoginpageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
